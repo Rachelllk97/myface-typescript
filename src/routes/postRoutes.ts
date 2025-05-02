39f7892bb0f93f85d5d7e1e271568b9255e10283
Rachel Kelly <rachel.kelly@softwire.com>
2025-05-02 12:32:18 +0100

    accessed image property

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -18,6 +18,7 @@ router.get("/", async (request, response) => {
     : 10;
 
   const postList = await getPageOfPosts(page, pageSize);
+  console.log(postList.results[0].likedBy[0].username)
   return response.render("post_list", {
     postList: postList,
     format: format,
